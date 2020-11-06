ac47b726e48ad2e4fbdee8b77ec612bcd594a304
zx <zxpstudy@163.com>
2020-11-06 15:19:37 +0800

    feat(react-demo): add DatasetDemo

--- example/demo-react/src/ChartDemo.tsx
+++ example/demo-react/src/ChartDemo.tsx
@@ -1,101 +1,47 @@
 import React, { useState } from "react";
-import {
-  BaseChart,
-  Chart,
-  Dataset,
-  Event,
-  Legend,
-  MethodResize,
-  Series,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "@echarts-start/react-bridge";
+import { map, get } from "lodash";
+import { BasicDemo } from "./component/BasicDemo";
+import { DatasetDemo } from "./component/DatasetDemo";
 
-const data = [
-  { product: "Matcha Latte", "2015": 43.3, "2016": 85.8, "2017": 93.7 },
-  { product: "Milk Tea", "2015": 83.1, "2016": 73.4, "2017": 55.1 },
-  { product: "Cheese Cocoa", "2015": 86.4, "2016": 65.2, "2017": 82.5 },
-  { product: "Walnut Brownie", "2015": 72.4, "2016": 53.9, "2017": 39.1 },
-];
+const ViewMap = {
+  BasicDemo: BasicDemo,
+  DatasetDemo: DatasetDemo,
+};
 
-const dimensions = ["product", "2015", "2016", "2017"];
-
-const Demo1 = () => {
-  const [loading, setLoading] = useState<boolean>(false);
+const Menu = ({ current, setCurrent }: { current: string; setCurrent: (c: string) => void }) => {
   return (
-    <div style={{ textAlign: "center" }}>
-      <a href={"#"} onClick={() => setLoading((prevState) => !prevState)}>
-        toggle loading
-      </a>
-      <Chart style={{ width: "100%", height: 400 }} resize loading={loading}>
-        <XAxis type={"category"} />
-        <YAxis />
-        <Tooltip trigger={"axis"} />
-        <Legend />
-        <Series type={"line"} />
-        <Series type={"line"} />
-        <Series type={"line"} />
-        <Dataset dimensions={dimensions} source={data} />
-        <Event
-          eventName={"click"}
-          eventHandler={(e) => {
-            console.log("click event=", e);
-          }}
-        />
-      </Chart>
+    <div style={{ display: "flex", flexDirection: "column", borderRight: "1px solid #ccc", height: "100vh" }}>
+      {map(ViewMap, (_, k) => {
+        return (
+          <div
+            key={k}
+            style={{
+              cursor: "pointer",
+              color: k === current ? "blue" : "#333",
+              padding: ".2em 1em",
+            }}
+            onClick={() => {
+              setCurrent(k);
+            }}>
+            {k}
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-const Demo2 = () => {
-  return (
-    <BaseChart style={{ width: "100%", height: 400 }}>
-      <XAxis type={"category"} />
-      <YAxis />
-      <Tooltip trigger={"axis"} />
-      <Legend />
-      <Series type={"line"} />
-      <Series type={"line"} />
-      <Series type={"line"} />
-      <Dataset dimensions={dimensions} source={data} />
-      <MethodResize resize />
-      <Event
-        eventName={"click"}
-        eventHandler={(e) => {
-          console.log("click event=", e);
-        }}
-      />
-    </BaseChart>
-  );
-};
-
-const Demo3 = () => {
-  return (
-    <Chart
-      resize
-      style={{ width: "100%", height: 400 }}
-      chartRef={(chart) => {
-        chart.setOption({
-          legend: {},
-          tooltip: { trigger: "axis" },
-          xAxis: { type: "category" },
-          yAxis: {},
-          series: [{ type: "line" }, { type: "line" }, { type: "line" }],
-          dataset: { dimensions, source: data },
-        });
-      }}
-    />
-  );
+const Content = ({ current }: { current: string }) => {
+  const V = get(ViewMap, current);
+  return <div style={{ flexGrow: 1, height: "100vh", overflow: "auto" }}>{V ? <V /> : null}</div>;
 };
 
 export const ChartDemo = () => {
+  const [current, setCurrent] = useState<string>("BasicDemo");
   return (
-    <div>
-      ChartDemo
-      <Demo1 />
-      <Demo2 />
-      <Demo3 />
+    <div style={{ display: "flex" }}>
+      <Menu current={current} setCurrent={setCurrent} />
+      <Content current={current} />
     </div>
   );
 };
---
+++ example/demo-react/src/component/BasicDemo.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import {
+  BaseChart,
+  Chart,
+  Dataset,
+  Event,
+  Legend,
+  MethodResize,
+  Series,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "../../../../@echarts-start/react-bridge/src";
+
+const data = [
+  { product: "Matcha Latte", "2015": 43.3, "2016": 85.8, "2017": 93.7 },
+  { product: "Milk Tea", "2015": 83.1, "2016": 73.4, "2017": 55.1 },
+  { product: "Cheese Cocoa", "2015": 86.4, "2016": 65.2, "2017": 82.5 },
+  { product: "Walnut Brownie", "2015": 72.4, "2016": 53.9, "2017": 39.1 },
+];
+
+const dimensions = ["product", "2015", "2016", "2017"];
+
+const Demo1 = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  return (
+    <div style={{ textAlign: "center" }}>
+      <a href={"#"} onClick={() => setLoading((prevState) => !prevState)}>
+        toggle loading
+      </a>
+      <Chart style={{ width: "100%", height: 400 }} resize loading={loading}>
+        <XAxis type={"category"} />
+        <YAxis />
+        <Tooltip trigger={"axis"} />
+        <Legend />
+        <Series type={"line"} />
+        <Series type={"line"} />
+        <Series type={"line"} />
+        <Dataset dimensions={dimensions} source={data} />
+        <Event
+          eventName={"click"}
+          eventHandler={(e) => {
+            console.log("click event=", e);
+          }}
+        />
+      </Chart>
+    </div>
+  );
+};
+
+const Demo2 = () => {
+  return (
+    <BaseChart style={{ width: "100%", height: 400 }}>
+      <XAxis type={"category"} />
+      <YAxis />
+      <Tooltip trigger={"axis"} />
+      <Legend />
+      <Series type={"line"} />
+      <Series type={"line"} />
+      <Series type={"line"} />
+      <Dataset dimensions={dimensions} source={data} />
+      <MethodResize resize />
+      <Event
+        eventName={"click"}
+        eventHandler={(e) => {
+          console.log("click event=", e);
+        }}
+      />
+    </BaseChart>
+  );
+};
+
+const Demo3 = () => {
+  return (
+    <Chart
+      resize
+      style={{ width: "100%", height: 400 }}
+      chartRef={(chart) => {
+        chart.setOption({
+          legend: {},
+          tooltip: { trigger: "axis" },
+          xAxis: { type: "category" },
+          yAxis: {},
+          series: [{ type: "line" }, { type: "line" }, { type: "line" }],
+          dataset: { dimensions, source: data },
+        });
+      }}
+    />
+  );
+};
+
+export const BasicDemo = () => {
+  return (
+    <div>
+      <div>Chart</div>
+      <Demo1 />
+      <div>BaseChart</div>
+      <Demo2 />
+      <div>Chart Ref</div>
+      <Demo3 />
+    </div>
+  );
+};
